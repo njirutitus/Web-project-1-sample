45117542e127c4854be9a6467d8fb26b9983b68d
Titus Njiru <njirutitus@gmail.com>
2020-05-17 21:38:17 +0300

    Added the javascript code for accordion
    I also added the following:
    The help button on the header
    changed from your account to login on the header
    added icons on both sides of login and help buttons
    Added the background color for header and footer.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // JavaScript Document
+var acc = document.getElementsByClassName("aside-accordion-item-heading");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+		/* Toggle between adding and removing the "active" class, 
+		to highlight the button that controls the panel */
+		this.classList.toggle("active");
+		
+		/* Toggle between hiding and showing the active panel */
+		var panel = this.nextElementSibling;
+		var toggle = this.firstElementChild;
+		var icon = toggle.firstElementChild;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+			icon.className = "fas fa-chevron-right";
+		} else {
+			panel.style.display = "block";
+			icon.className = "fas fa-chevron-down";
+		}
+	});
+}
